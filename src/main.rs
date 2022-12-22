1d8d7a546a866217beda5fe9dda4b082a4963f9f
Maphikza <stapisi@protonmail.com>
2022-12-22 15:38:02 +0200

    Adding error handling and formatting for the printing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
 #[derive(Debug)]
 struct Question {
-    id: QuestionId,
-    title: String,
-    content: String,
-    tags: Option<Vec<String>>,
+    _id: QuestionId,
+    _title: String,
+    _content: String,
+    _tags: Option<Vec<String>>,
 }
 #[derive(Debug)]
 struct QuestionId(String);
 
 impl Question {
-    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
-        Self {
-            id,
-            title,
-            content,
-            tags,
+    fn new(_id: QuestionId, _title: String, _content: String, _tags: Option<Vec<String>>) -> Self {
+        Question {
+            _id,
+            _title,
+            _content,
+            _tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
         }
     }
 }
 
 fn main() {
-    let first_question = QuestionId("Why".to_string());
+    // let first_question = QuestionId("Why".to_string());
     let question = Question::new(
-        first_question,
+        QuestionId::from_str("Why").expect("No id provided"),
         "First Question".to_string(),
         "Question content".to_string(),
         Some(vec!["faq".to_string()]),
     );
 
-    println!(
-        "{:?}, {}, {}, {:?}.",
-        question.id.0, question.title, question.content, question.tags.unwrap()[0]
-    );
+    println!("{:?}.", question);
 }
